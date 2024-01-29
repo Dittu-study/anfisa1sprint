0d04bf317febab3d5a25f5e68be089b9a74ebde0
Данил Казаченко <ditussimulus@gmail.com>
2024-01-29 21:28:28 +0300

    Добавление тела проекта х2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    template = 'blog/index.html'
+    return render(request, template)
+
+
+def post_detail(request, pk):
+    template = 'detail/html'
+    context = 
+    return render(request, template, context)
+
+
+def category_posts(request, pk):
+    template = 'category.html'
+    context = 
+    return render(request, template, context)
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def about(request):
+    template = 'about/html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'rules/html'
+    return render(request, template)
